e2909489dc6b7c29214270a4246cb6289ac3dd52
Wolfsion <jison.c9@qq.com>
2024-06-29 22:51:19 +0800

    admin-user

--- src/views/admin/AdminUserView.vue
+++ src/views/admin/AdminUserView.vue
@@ -1,7 +1,148 @@
 <template>
-  <h2>你好啊</h2>
+  <a-form
+    :model="formSearchParams"
+    :style="{
+      justifyContent: 'center',
+      marginBottom: '20px',
+    }"
+    label-align="left"
+    auto-label-width
+    layout="inline"
+    @submit="searchUser"
+  >
+    <a-form-item field="userName" label="用户名">
+      <a-input v-model="formSearchParams.userName" placeholder="请输入用户名" />
+    </a-form-item>
+    <a-form-item>
+      <a-button type="primary" html-type="submit" style="width: 120px">
+        搜索
+      </a-button>
+    </a-form-item>
+  </a-form>
+  <a-table
+    :columns="userColumns"
+    :data="dataSource"
+    :pagination="{
+      showTotal: true,
+      pageSize: searchParams.pageSize,
+      current: searchParams.current,
+      total,
+    }"
+    @page-change="onPageChange"
+  >
+    <template #userAvatar="{ record }">
+      <a-image width="64" :src="record.userAvatar" />
+    </template>
+    <template #optional="{ record }">
+      <a-space>
+        <a-button type="primary" @click="updateUser(record)">修改</a-button>
+        <a-button status="danger" @click="deleteUser(record)">删除</a-button>
+      </a-space>
+    </template>
+  </a-table>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, watchEffect } from "vue";
+import API from "@/api";
+import {
+  deleteUserUsingPost,
+  listUserVoByPageUsingPost,
+} from "@/api/userController";
+import message from "@arco-design/web-vue/es/message";
+
+const dataSource = ref<API.User[]>([]);
+const total = ref<number>();
+const formSearchParams = ref<API.UserQueryRequest>({});
+const initSearchParams = {
+  pageSize: 5,
+  current: 1,
+};
+const searchParams = ref<API.UserQueryRequest>({
+  ...initSearchParams,
+  ...formSearchParams.value,
+});
+
+const userColumns = [
+  {
+    title: "ID",
+    dataIndex: "id",
+  },
+  {
+    title: "账户",
+    dataIndex: "userAccount",
+  },
+  {
+    title: "用户名",
+    dataIndex: "userName",
+  },
+  {
+    title: "用户头像",
+    dataIndex: "userAvatar",
+    slotName: "userAvatar",
+  },
+  {
+    title: "用户简介",
+    dataIndex: "userProfile",
+  },
+  {
+    title: "权限",
+    dataIndex: "userRole",
+  },
+  {
+    title: "创建时间",
+    dataIndex: "createTime",
+  },
+  {
+    title: "更新时间",
+    dataIndex: "updateTime",
+  },
+  {
+    title: "操作",
+    slotName: "optional",
+  },
+];
+
+const refreshData = async () => {
+  const res = await listUserVoByPageUsingPost(searchParams.value);
+  if (res.data.code === 0) {
+    dataSource.value = res.data.data?.records || [];
+    total.value = res.data.data?.total || 0;
+  } else {
+    message.error("获取数据失败");
+  }
+};
+
+watchEffect(() => {
+  refreshData();
+});
+
+const updateUser = (record: API.User) => {
+  console.log("updateData", record);
+};
+
+const deleteUser = async (record: API.User) => {
+  const res = await deleteUserUsingPost({ id: record.id });
+  if (res.data.code === 0) {
+    await refreshData();
+  } else {
+    message.error("删除失败");
+  }
+};
+
+const searchUser = async () => {
+  searchParams.value = {
+    ...initSearchParams,
+    ...formSearchParams.value,
+  };
+};
+
+const onPageChange = (page: number) => {
+  searchParams.value = {
+    ...searchParams.value,
+    current: page,
+  };
+};
+</script>
 
 <style scoped></style>
